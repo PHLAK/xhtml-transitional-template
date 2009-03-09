13af2d2aeb31107267842401d8d15516f08b4962
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2009-03-09 01:20:41 -0700

    First commit

---
+++ index.php
@@ -0,0 +1,102 @@
+<?php
+  // Get current page file name ***REQUIRED FOR DYNAMIC MENU***
+  $page = basename($_SERVER["PHP_SELF"]);
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <title>Home | Site Title</title>
+  <link rel="stylesheet" type="text/css" href="css/reset.css" />
+  <link rel="stylesheet" type="text/css" href="css/style.css" />
+  <!--[if IE]><link rel="stylesheet" type="text/css" href="css/ie-fixes.css" /><![endif]-->
+  <link rel="shortcut icon" href="images/favicon.gif" />
+  <script type="text/javascript" src="js/jquery.js"></script>
+  <script type="text/javascript" src="js/jquery.corners.js"></script>
+  <script type="text/javascript">
+    $(function(){
+      $('div.rounded').corner();
+    });
+  </script>
+</head>
+
+<body>
+<div id="wrapper">
+
+  <div id="header">
+    <h1>Site Title</h1>
+  </div>
+
+
+  <ul id="navigation">
+    <li><a href="index.php">Home</a></li>
+    <li><a href="#">Link</a></li>
+    <li><a href="#">Link</a></li>
+    <li><a href="#">Link</a></li>
+    <li><a href="#">Link</a></li>
+  </ul>
+
+
+  <div id="content">
+    <h1>This is an h1 title</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla semper odio. Vivamus vitae
+    est. Donec magna magna, tincidunt non, tempus et, feugiat ut, arcu. Duis volutpat eleifend
+    sapien. Cras fringilla mauris at magna. Donec ornare. Morbi mollis urna eu risus. Quisque eros.
+    Nullam lacus enim, hendrerit ac, congue et, ornare ac, nisi. Class aptent taciti sociosqu ad
+    litora torquent per conubia nostra, per inceptos himenaeos. Nunc nec purus eget pede pretium
+    volutpat. Quisque interdum orci et orci scelerisque semper. Nullam eu arcu. Pellentesque ligula.
+    Quisque eros lectus, consequat a, tempus at, placerat ut, odio.</p>
+
+    <h2>This is an h2 title</h2>
+    <p>Integer porttitor semper urna. Etiam eu tortor. Morbi eget est. Suspendisse euismod cursus
+    purus. Vestibulum elit. Maecenas quis eros at lorem dignissim fringilla. Fusce elementum
+    pharetra erat. Sed ut ipsum. Integer ac arcu. Suspendisse quis augue vel sem hendrerit
+    ultricies. Nulla id felis quis mauris eleifend ultricies. Vestibulum lacus diam, ullamcorper
+    varius, adipiscing ut, rutrum vestibulum, ligula.</p>
+
+    <h3>This is an h3 title</h3>
+    <p>Aliquam feugiat imperdiet purus. Donec lacus. Quisque nisl nunc, dictum at, bibendum non,
+    tempor in, ipsum. In rutrum massa in ligula. Nunc turpis justo, ultrices quis, pellentesque ac,
+    faucibus vitae, sapien. Mauris tellus pede, commodo semper, ultrices eu, ultricies a, eros.
+    Integer blandit turpis at neque. Donec bibendum. Aliquam erat volutpat. Aliquam erat volutpat.
+    Suspendisse in libero. Integer vehicula nisi id libero. Curabitur cursus quam blandit dui.
+    Proin libero nisi, volutpat sit amet, eleifend sed, malesuada vel, dui. Vivamus accumsan, eros
+    sed tincidunt mollis, orci est rutrum dui, in interdum urna neque pellentesque sapien. Nulla
+    nec dui nec mi vulputate ullamcorper.</p>
+  </div>
+
+
+  <div id="footer">
+    <span class="left">&copy; <?php echo(date('Y'));?>
+      <strong>&middot;</strong> Chris Kankiewicz
+      <strong>&middot;</strong> <a href="legal.php">Some Rights Reserved</a>
+    </span>
+    <span class="right">
+      <?php
+        if (file_exists('sitemap.xml')) {
+          echo "<a href=\"sitemap.xml\" target=\"_blank\">Sitemap</a> <strong>&middot;</strong> ";
+        }
+      ?>
+      Design by, <a href="http://www.web-geek.net">Chris Kankiewicz</a>
+    </span>
+  </div>
+
+</div>
+
+<!-- Page design by, Chris Kankiewicz <http://www.web-geek.net> -->
+
+<!--
+0100100100100000011001000110111100100000011011100110111101110100
+0010000001100110011001010110000101110010001000000110001101101111
+0110110101110000011101010111010001100101011100100111001100101110
+0010000001001001001000000110011001100101011000010111001000100000
+0111010001101000011001010010000001101100011000010110001101101011
+0010000001101111011001100010000001110100011010000110010101101101
+0010111000100000001011010010000001001001011100110110000101100001
+0110001100100000010000010111001101101001011011010110111101110110
+-->
+
+</body>
+</html>
